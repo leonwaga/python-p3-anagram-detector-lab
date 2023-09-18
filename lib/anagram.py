3b6e5084c241b8bd24a088adfc98df31702a3952
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-09-18 23:32:24 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagram):
+        anagrams = []
+        sorted_word = sorted(self.word)
+
+        for words in possible_anagram:
+            words = words.lower()
+
+        
+            if words != self.word and sorted(words) == sorted_word:
+                anagrams.append(words)
+
+        return anagrams
+
